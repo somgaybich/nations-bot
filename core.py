f9f8403ef2cef6f3c4054e268bf7a33780827d3c
somgaybich <imbow568@gmail.com>
2025-12-22 20:05:29 -0500

    Improved Logging

--- core.py
+++ core.py
@@ -7,9 +7,22 @@ import logging
 # Clears preexisting log data
 with open("logs/last.log", 'w') as f:
     pass
-logging.basicConfig(filename='logs/last.log', encoding='utf-8', level=logging.DEBUG, 
-                    format="[%(asctime)s][%(levelname)s] [%(message)s]", datefmt='%H:%M:%S')
+
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("[%(asctime)s][%(levelname)s] [%(message)s]", datefmt='%H:%M:%S')
+
+file_handler = logging.FileHandler('logs/last.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG) # For now set to DEBUG, if it becomes clogged change to INFO
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 from scripts.botlib import bot
 
