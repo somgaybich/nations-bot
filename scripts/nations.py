3005422ef90ec9faf79f3b7f04e6195de810662e
somgaybich <imbow568@gmail.com>
2026-01-06 20:38:59 -0500

    Added espionage
    Added the Espionage class and all its functionality.

--- scripts/nations.py
+++ scripts/nations.py
@@ -1,6 +1,7 @@
 import json
 import math
 import logging
+import random
 from discord import Embed, Color
 
 from scripts.constants import current_season, update_season, json_terrain
@@ -273,6 +274,26 @@ class Econ:
                     cap += 2
 
         return 1
+
+class Espionage:
+    def __init__(self, investment, espionage_type, target):
+        self.investment = investment
+        self.success_chance = 0.1 + (0.05 * (investment - 1))
+        self.reveal_chance = 0.9 - (0.04 * (investment - 1))
+        self.espionage_type = espionage_type
+        self.target = target
+    
+    def roll(self):
+        if random.random() > self.success_chance:
+            if self.espionage_type == "spy":
+                pass
+                # do spy stuff?
+            elif self.espionage_type == "assassin":
+                pass
+                # do assassin stuff?
+        if random.random() > self.reveal_chance:
+            pass
+            # do reveal stuff?
 
 class Nation:
     """
