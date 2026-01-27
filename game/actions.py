65c7172a053ddb20cc55aa75055a6c83c8e18ca5
somgaybich <imbow568@gmail.com>
2026-01-27 17:00:42 -0500

    Added admin mode

--- game/actions.py
+++ game/actions.py
@@ -5,6 +5,7 @@ logger = logging.getLogger(__name__)
 
 import scripts.errors as errors
 
+from game.constants import admin_mode
 from game.military import Unit
 
 from world.map import Tile, hex_distance
@@ -30,43 +31,43 @@ async def new_link(path: list[Tile], linktype: LinkType, owner: int, origin: Cit
             raise errors.InvalidLocation("Link construction", "in water tiles")
 
     resources = origin.inventory + destination.inventory
-    if resources.count("metal") < metal_cost:
+    if resources.count("metal") < metal_cost and not admin_mode:
         raise errors.NotEnoughResources("Link construction", ["metal"] * metal_cost, resources)
-    if resources.count("stone") < stone_cost:
+    if resources.count("stone") < stone_cost and not admin_mode:
         raise errors.NotEnoughResources("Link construction", ["stone"] * stone_cost, resources)
-    if econ.influence < inf_cost:
+    if econ.influence < inf_cost and not admin_mode:
         raise errors.NotEnoughInfluence("Link construction", inf_cost, econ.influence)
     
-    metal_remaining = metal_cost
-    last = 0
-    while metal_remaining > 0:
-        if last == 0:
-            if "metal" in origin.inventory:
-                origin.inventory.remove("metal")
-            last = 1
-        elif last == 1:
-            if "metal" in destination.inventory:
-                destination.inventory.remove("metal")
-            last = 0
-        metal_remaining -= 1
+    if not admin_mode:
+        metal_remaining = metal_cost
+        last = 0
+        while metal_remaining > 0:
+            if last == 0:
+                if "metal" in origin.inventory:
+                    origin.inventory.remove("metal")
+                last = 1
+            elif last == 1:
+                if "metal" in destination.inventory:
+                    destination.inventory.remove("metal")
+                last = 0
+            metal_remaining -= 1
         
-    stone_remaining = stone_cost
-    last = 0
-    while stone_remaining > 0:
-        if last == 0:
-            if "stone" in origin.inventory:
-                origin.inventory.remove("stone")
-            last = 1
-        elif last == 1:
-            if "stone" in destination.inventory:
-                destination.inventory.remove("stone")
-            last = 0
-        stone_remaining -= 1
+        stone_remaining = stone_cost
+        last = 0
+        while stone_remaining > 0:
+            if last == 0:
+                if "stone" in origin.inventory:
+                    origin.inventory.remove("stone")
+                last = 1
+            elif last == 1:
+                if "stone" in destination.inventory:
+                    destination.inventory.remove("stone")
+                last = 0
+            stone_remaining -= 1
 
-    econ.influence -= inf_cost
+        econ.influence -= inf_cost
 
-    for location in path:
-        tile = tile_list[location]
+    for tile in path:
         if hex_distance(tile, origin) <= hex_distance(tile, destination):
             tile.structures.append(Structure(linktype, location, origin, owner))
         else:
@@ -89,14 +90,15 @@ async def new_army(name: str, userid: int, city_name: str) -> Unit:
 
     if city is None:
         raise errors.DoesNotExist("city", "Army creation", city_name)
-    if econ.influence < 1:
+    if econ.influence < 1 and not admin_mode:
         return errors.NotEnoughInfluence("Army creation", 1, econ.influence)
     
     base_strength = 1
     if city.structures.has("Foundry"):
         base_strength = 1.3
 
-    econ.influence -= 1
+    if not admin_mode:
+        econ.influence -= 1
     new_unit = Unit(name=name, type="army", location=city.location, strength=base_strength, movement_free=3, owner=userid, home=city_name)
     nation.military[name] = new_unit
     units.append(new_unit)
@@ -116,14 +118,14 @@ async def new_fleet(name: str, userid: int, city_name: str) -> Unit:
         raise errors.DoesNotExist("city", "Fleet creation", city_name)
     if not city.structures.has("Port"):
         raise errors.MissingStructure("Fleet creation", "Port")
-    if econ.influence < 2:
+    if econ.influence < 2 and not admin_mode:
         raise errors.NotEnoughInfluence("Fleet creation", 2, econ.influence)
     
     base_strength = 1
     if city.structures.has("Foundry"):
         base_strength = 1.3
-    
-    econ.influence -= 2
+    if not admin_mode:
+        econ.influence -= 2
     new_unit = Unit(name=name, type="fleet", location=city.location, strength=base_strength, movement_free=6, owner=userid, home=city_name)
     nation.military[name] = new_unit
     units.append(new_unit)
@@ -154,11 +156,11 @@ async def new_city(name: str, location: tuple[int, int], owner: int, capital: bo
             to_be_claimed.append(tile.location)
         elif tile.owner == owner:
             continue
-        # Tile is owned by another player
         else:
+            # Tile is owned by another player
             raise errors.NotOwned('Settlement creation', tile.location)
 
-    if not capital:
+    if not capital and not admin_mode:
         in_range = False
         for unit in units:
             if hex_distance(unit.location, city_tile) <= 1:
@@ -170,9 +172,8 @@ async def new_city(name: str, location: tuple[int, int], owner: int, capital: bo
                     in_range = True
         if not in_range:
             raise errors.InvalidLocation("Settlement creation", "too far from settlements or units")
-
-    # This check must be last because it has behavior!
-    if not capital:
+        
+        # This check must be last because it has behavior!
         if nation.econ.influence < 4:
             raise errors.NotEnoughInfluence('Settlement creation', 4, nation.econ.influence)
         else:
@@ -220,9 +221,9 @@ async def new_structure(structure_type: StructureType, location: tuple[int, int]
         raise errors.TooManyStructures(f"{structure_type.name} creation", 2)
     if len(city.structures) == 3 and not city.tier == 4:
         raise errors.TooManyStructures(f"{structure_type.name} creation", 3)
-    if structure_type.resource_cost not in city.inventory:
+    if structure_type.resource_cost not in city.inventory and not admin_mode:
         raise errors.NotEnoughResources(f"{structure_type.name} creation", structure_type.resource_cost, city.inventory)
-    if nation_list[builder].econ.influence < structure_type.inf_cost:
+    if nation_list[builder].econ.influence < structure_type.inf_cost and not admin_mode:
         raise errors.NotEnoughInfluence(f"{structure_type.name} creation", structure_type.inf_cost, econ.influence)
     if structure_type.usable_in == ["city"]:
         if not isinstance(tile, City):
@@ -233,7 +234,7 @@ async def new_structure(structure_type: StructureType, location: tuple[int, int]
         raise errors.InvalidLocation(f"{structure_type.name} creation", "outide the settlement's range")
     if tile not in city.metroarea() and city.tier == 4:
         raise errors.InvalidLocation(f"{structure_type.name} creation", "outide the settlement's range")
-    if not structure_type.tier_req == 0 and location == city.location:
+    if not structure_type.tier_req == 0 and location == city.location and not admin_mode:
         if tile.tier < structure_type.tier_req:
             raise errors.CityTierTooLow(f"{structure_type.name} creation", tile.tier, structure_type.tier_req)
         
@@ -253,9 +254,10 @@ async def new_structure(structure_type: StructureType, location: tuple[int, int]
                 if index == len(tile.structures):
                     raise errors.MissingStructure(structure_type.name + " creation", structure_type.name)
 
-    for item in structure_type.resource_cost:
-        city.inventory.remove(item)
-    econ.influence -= structure_type.inf_cost
+    if not admin_mode:
+        for item in structure_type.resource_cost:
+            city.inventory.remove(item)
+        econ.influence -= structure_type.inf_cost
 
     new_structure = Structure(structure_type, location, root_city, builder)
     tile.structures.append(new_structure)
